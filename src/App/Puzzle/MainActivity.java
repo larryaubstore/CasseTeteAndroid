781f673ae15ce480a9a8c23299a83f1e0a389cb4
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2013-03-23 21:30:54 -0400

    Défilement horizontale et verticale désactivés.

--- src/App/Puzzle/MainActivity.java
+++ src/App/Puzzle/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
 import android.webkit.WebSettings.RenderPriority;
+import android.view.View;
+import 	android.view.MotionEvent;
 
 public class MainActivity extends Activity
 {
@@ -21,7 +23,15 @@ public class MainActivity extends Activity
 				webSettings.setJavaScriptEnabled(true);
 				webSettings.setRenderPriority(RenderPriority.HIGH);
 				webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+				
+				mainWebView.setHorizontalScrollBarEnabled(false);
+				mainWebView.setVerticalScrollBarEnabled(false);
 
+				mainWebView.setOnTouchListener(new View.OnTouchListener() {
+			    public boolean onTouch(View v, MotionEvent event) {
+			      return (event.getAction() == MotionEvent.ACTION_MOVE);
+			    }
+			  });
 
         mainWebView.loadUrl("file:///android_asset/index.php.html");
 
